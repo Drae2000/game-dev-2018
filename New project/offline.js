927426525063cec7202068c994bba19c3f6e2a48
Drae2000 <36052899+Drae2000@users.noreply.github.com>
2018-05-14 12:40:48 -0700

    Add files via upload

---
+++ New project/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1526066773,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bola-sheet0.png",
+		"images/piso-sheet0.png",
+		"images/lineamortal-sheet0.png",
+		"images/leftscreen-sheet0.png",
+		"images/rightscreen-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"loading-logo.png",
+		"icon-256.png"
+	]
+}
